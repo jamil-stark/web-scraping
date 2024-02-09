34d52e8181963c7ec31fdf42b4f742829d5603fa
jamil-stark <kunoomaking@gmail.com>
2024-02-09 03:00:42 +0300

    extracted subtitles and their content

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ with open('index.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    library_titles = soup.find_all('div', class_='col-md-6')
-    for library_title in library_titles:
-        print(str(library_title.div.div.h5) + '\n\n\n\n')
+    card_bodies = soup.find_all('div', class_='card-body')
+    for card_body in card_bodies:
+        print(card_body.h5.text)
+        print(card_body.p.text)
+        print()
